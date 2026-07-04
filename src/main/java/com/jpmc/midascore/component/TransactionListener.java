974efa65322c680cfa97e4c967f254d354121534
Sarrah Barazi <Sarrahbarazi5@gmail.com>
2026-07-03 22:12:22 -0400

    task 3: integrate  spring application with H2 database

--- src/main/java/com/jpmc/midascore/component/TransactionListener.java
+++ src/main/java/com/jpmc/midascore/component/TransactionListener.java
@@ -1,11 +1,38 @@
 package com.jpmc.midascore.component;
 import org.springframework.stereotype.Component;
+
+import com.jpmc.midascore.entity.TransactionRecord;
+import com.jpmc.midascore.entity.UserRecord;
 import com.jpmc.midascore.foundation.Transaction;
+import com.jpmc.midascore.repository.TransactionRecordRepository;
+import com.jpmc.midascore.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 
 @Component
 public class TransactionListener {
+     @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TransactionRecordRepository transactionRecordRepository;
+
     @KafkaListener(topics = "${general.kafka-topic}", groupId = "midascore-group")
     public void listen(Transaction transaction) {
+        UserRecord sender = userRepository.findById(transaction.getSenderId());
+        UserRecord recipient = userRepository.findById(transaction.getRecipientId());
+
+        if (sender == null || recipient == null) return;
+        if (sender.getBalance() < transaction.getAmount()) return;
+
+        sender.setBalance(sender.getBalance() - transaction.getAmount());
+        recipient.setBalance(recipient.getBalance() + transaction.getAmount());
+
+        userRepository.save(sender);
+        userRepository.save(recipient);
+
+        transactionRecordRepository.save(new TransactionRecord(sender, recipient, transaction.getAmount()));
+        
     }
 }
--- src/test/java/com/jpmc/midascore/TaskThreeTests.java
+++ src/test/java/com/jpmc/midascore/TaskThreeTests.java
@@ -1,5 +1,6 @@
 package com.jpmc.midascore;
 
+import com.jpmc.midascore.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,6 +23,9 @@ public class TaskThreeTests {
 
     @Autowired
     private FileLoader fileLoader;
+
+    @Autowired
+    UserRepository userRepository;
 
     @Test
     void task_three_verifier() throws InterruptedException {
